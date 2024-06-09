d37c9cefe33fc18e6cf4fe28713bdf578c45f8c0
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-06-10 01:32:59 +0700

    fix: @vercel speed insight component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 function App() {
   return (
-    <div className="flex h-screen bg-slate-200 dark:bg-slate-800">
-      <div className="w-screen self-center">
-        <div className="text-center text-2xl dark:text-slate-300">
-          <p className="font-fkDisplay"> Hello, World!</p>
-          <p>Hello, World!</p>
-          <p className="font-serif">Hello, World!</p>
-          <p className="font-mono">Hello, World!</p>
-          <p>เฮลโล, เวิลด์!</p>
-          <p>こんにちは、世界！</p>
-          <p>안녕세계</p>
-          <p>你好世界</p>
+    <>
+      <div className="flex h-screen bg-slate-200 dark:bg-slate-800">
+        <div className="w-screen self-center">
+          <div className="text-center text-2xl dark:text-slate-300">
+            <p className="font-fkDisplay"> Hello, World!</p>
+            <p>Hello, World!</p>
+            <p className="font-serif">Hello, World!</p>
+            <p className="font-mono">Hello, World!</p>
+            <p>เฮลโล, เวิลด์!</p>
+            <p>こんにちは、世界！</p>
+            <p>안녕세계</p>
+            <p>你好世界</p>
+          </div>
         </div>
       </div>
-    </div>
+      <SpeedInsights />
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -7,6 +6,5 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <SpeedInsights />
   </React.StrictMode>,
 );

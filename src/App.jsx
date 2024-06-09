7f2f929ff818973abc08cd33134c7e0e0fd38102
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-06-10 01:37:02 +0700

    add: @vercel analytics component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         </div>
       </div>
       <SpeedInsights />
+      <Analytics />
     </>
   );
 }

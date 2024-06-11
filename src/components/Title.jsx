123b2cabf7e1a87fe6565dde9452d54d8952ede0
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-06-11 21:31:39 +0700

    style: fix title text margin

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -8,7 +8,7 @@ function Title() {
           className="mr-3 size-20 self-center rounded-md max-sm:size-32"
         />
         <div className="self-center text-start">
-          <h1 className="text-balances font-fkDisplay text-3xl underline underline-offset-4">
+          <h1 className="text-balances font-fkDisplay text-3xl underline underline-offset-4 max-sm:mb-3">
             Theppitak M.
           </h1>
           <p className="font-mono font-light leading-tight">

8ab3dd4b1a5d83a27bdcb6f1f0097e8b02e02a30
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-06-11 15:42:48 +0700

    style: fix header bar position

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="flex h-12 w-screen justify-between bg-gray-700/20 shadow ring-1 ring-black/5 backdrop-blur-lg">
+    <div className="flex h-12 w-screen justify-between bg-gray-700/50 shadow backdrop-blur-lg">
       <img
         src="/logo.png"
         alt="avatar"

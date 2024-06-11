4043d829e70486655691a03dcfcc9ca79616d652
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-06-12 01:59:19 +0700

    style: add horizontal padding for tags

--- src/sub-components/StacksTag.jsx
+++ src/sub-components/StacksTag.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function StacksTag({ stack }) {
   return (
     <div
-      className={`flex w-fit gap-x-1 rounded-xl text-gray-200 max-sm:my-1 ${stack.color} px-2 text-sm font-light`}
+      className={`flex w-fit gap-x-1 rounded-xl text-gray-200 max-sm:my-1 ${stack.color} px-2 py-1 text-sm font-light`}
     >
       <div className="self-center">
         {React.createElement(stack.reactIcon, { size: 12 })}
       </div>
-      {stack.name}
+      <p className="text-xs">{stack.name}</p>
     </div>
   );
 }

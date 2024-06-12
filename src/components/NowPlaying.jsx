fb0fcab4d4b25f7c208258d833ec0683f7d9ae43
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-06-12 15:20:44 +0700

    style: NowPlaying bar styling

--- src/components/NowPlaying.jsx
+++ src/components/NowPlaying.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { BiSolidBarChartAlt2 } from "react-icons/bi";
 import getNowPlayingItem from "../apis/spotify";
 
 export const NowPlaying = (props) => {
@@ -19,24 +20,36 @@ export const NowPlaying = (props) => {
   }, [props.client_id, props.client_secret, props.refresh_token]);
 
   return (
-    <div>
-      {loading && <p>Loading...</p>}
+    <div className="flex h-8 w-screen justify-center bg-emerald-950/60 text-slate-300 shadow backdrop-blur-md">
+      {loading && (
+        <p className="w-fit self-center text-center text-xs">Loading...</p>
+      )}
       {!loading && !isPlaying && (
-        <div>
-          <span>Currently offline</span>
+        <div className="my-2 flex w-fit self-center truncate">
+          <div className="mx-1 self-center">
+            <BiSolidBarChartAlt2 size={20} />
+          </div>
+          <p className="w-fit self-end text-xs">Currently Offline</p>
         </div>
       )}
       {!loading && isPlaying && (
-        <div>
-          <div>
-            <span>Now playing</span>
+        <div className="my-2 flex h-full w-fit self-center truncate">
+          <div className="flex self-center">
+            <p className="mr-1 self-end text-xs">Listening to</p>
+            <BiSolidBarChartAlt2 size={20} />
           </div>
-          <div>
-            <img src={result.albumImageUrl} alt={`${result.title} album art`} />
-            <a href={result.songUrl} target="_blank">
-              {result.title}
-            </a>
-            <p>{result.artist}</p>
+          <div className="flex self-center">
+            <img
+              src={result.albumImageUrl}
+              alt={`${result.title} album art`}
+              className="mx-1 size-5 self-end rounded-md"
+            />
+            <div className="flex self-end text-xs font-bold">
+              <a href={result.songUrl} target="_blank" className="font-bold">
+                {result.title}
+              </a>
+              <p className="ml-1">• {result.artist}</p>
+            </div>
           </div>
         </div>
       )}
